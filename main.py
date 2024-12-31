2dbaaaa91dc1f4f7509eaf82eddd2259f66f42e9
Duttonn <natao.dutton@gmail.com>
2024-12-31 15:24:34 +0100

    added https

--- main.py
+++ main.py
@@ -718,4 +718,11 @@ async def meteo(request: Request):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run(
+        "main:app",
+        host="0.0.0.0",
+        port=8000,
+        reload=True,
+        ssl_keyfile="localhost-key.pem",
+        ssl_certfile="localhost.pem"
+    )
